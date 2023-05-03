4285814efa695febf9d651eb7767b39de53f7781
ifejinity <mrlonzanida08@gmail.com>
2023-05-03 23:24:27 +0800

    partial hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    // fontSize: {
+    //   'sm': '0.8rem',
+    //   'base': '1rem',
+    //   'xl': '1.25rem',
+    //   '2xl': '1.563rem',
+    //   '3xl': '1.953rem',
+    //   '4xl': '2.441rem',
+    //   '5xl': '3.052rem',
+    // },
+    colors:{
+      'theme-black': '#1D242B',
+      'theme-white': '#FAFAFA',
+      'theme-lblue': '#C7EEFF',
+      'theme-dblue': '#0077C0',
+    },
     extend: {
       gridTemplateColumns: {
         'navDesktop': '1fr auto',
