2d20a41673c505ac1b93e1653edc910a8732bbde
ifejinity <mrlonzanida08@gmail.com>
2023-05-05 16:00:56 +0800

    darkmode addedd

--- index.js
+++ index.js
@@ -9,3 +9,46 @@ navbarCheckbox.addEventListener("click", function(){
         navbarMobile.style.right = "-100vh";
     }
 });
+
+// Darkmode function
+const modeToggle = document.querySelectorAll(".input");
+for (let modeToggleAll of modeToggle){
+    modeToggleAll.addEventListener("click", function(){
+        if(this == modeToggle[0]){
+            if(modeToggle[0].checked){
+                localStorage.theme = 'dark';
+                document.documentElement.classList.add('dark');
+                modeToggle[1].checked = true;
+            }
+            else{
+                localStorage.theme = 'light';
+                document.documentElement.classList.remove('dark');
+                modeToggle[1].checked = false;
+            }
+        }
+
+        if(this == modeToggle[1]){
+            if(modeToggle[1].checked){
+                localStorage.theme = 'dark';
+                document.documentElement.classList.add('dark');
+                modeToggle[0].checked = true;
+            }
+            else{
+                localStorage.theme = 'light';
+                document.documentElement.classList.remove('dark');
+                modeToggle[0].checked = false;
+            }
+        }
+    });
+}
+
+window.addEventListener("load", function(){
+    if(localStorage.theme === 'dark'){
+        document.documentElement.classList.add('dark');
+        modeToggle[0].checked = true;
+        modeToggle[1].checked = true;
+    }
+    else{
+        document.documentElement.classList.remove('dark');
+    }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./**/*.{html,js}"],
+  scripts: {
+    'dev': 'npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch',
+  },
   theme: {
-    // fontSize: {
-    //   'sm': '0.8rem',
-    //   'base': '1rem',
-    //   'xl': '1.25rem',
-    //   '2xl': '1.563rem',
-    //   '3xl': '1.953rem',
-    //   '4xl': '2.441rem',
-    //   '5xl': '3.052rem',
-    // },
-    colors:{
-      'theme-black': '#1D242B',
-      'theme-white': '#FAFAFA',
-      'theme-lblue': '#C7EEFF',
-      'theme-dblue': '#0077C0',
-    },
     extend: {
       gridTemplateColumns: {
         'navDesktop': '1fr auto',
         'navDesktopBtn': 'repeat(5, auto)',
+      },
+      colors:{
+        'theme-black': '#1D242B',
+        'theme-white': '#FAFAFA',
+        'theme-lblue': '#C7EEFF',
+        'theme-dblue': '#0077C0',
       },
       width: {
         'logo': '50px',
